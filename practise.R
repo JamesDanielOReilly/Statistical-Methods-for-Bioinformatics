77d741a3033d196abdfe44fe9ea6be4543b30802
James <jamesdanieloreilly@gmail.com>
2020-02-13 00:18:45 +0100

    finished correlation and covariance

--- practise.R
+++ practise.R
@@ -1,7 +1,16 @@
-## Installing necessary packages
+## libraries
 library(pastecs)
+
 ## Reading the data
 kalama = read.table("kalama.txt", header=T)
 
+## Descriptive statistics
+options(digits=2)
 descrip.kalama <- stat.desc(kalama[,c("age", "height")], basic = TRUE, desc = TRUE)
 
+## Calculating the covariance and correlation
+cov.age.height <- cov(kalama$age, kalama$height)
+cor.age.height <- cor(kalama$age, kalama$height)
+
+## Testing if the population correlation is zero
+corr.age.height.test <- cor.test(kamala$age, kalama$height, alternative="two.sided", method = "pearson")
